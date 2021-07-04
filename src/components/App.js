ddfd36ecb71b18d3deff8097514ad517a3e7ce06
haguse <halitguvens@gmail.com>
2021-07-04 15:36:03 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,6 @@ class App extends Component {
     const response = await axios.get(
       `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`
     );
-    console.log(response.data.results);
     this.setState({ movies: response.data.results });
   }
 
